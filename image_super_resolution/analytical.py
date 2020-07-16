989709890eae7b0efaabf7def63d2dca053d9b09
ANAS ESSOUNAINI <essounaini97@gmail.com>
2020-07-16 12:20:44 +0200

    - formatting and documenting the code

--- image_super_resolution/analytical.py
+++ image_super_resolution/analytical.py
@@ -9,30 +9,57 @@ import matplotlib.pyplot as plt
 from time import time
 from image_super_resolution.utils import *
 
-def HXconv(x,B):
-    m,n = x.shape
-    m0,n0 = B.shape
-    
-    Bpad = np.zeros((m,n))
-    Bpad[int((m-m0)/2):int((m+m0)/2),int((n-n0)/2):int((n+n0)/2)] = B
+
+def HXconv(x, B):
+    """[summary]
+
+    Args:
+        x (np.array): High resolution image
+        B (np.array): Blurring kernel
+
+    Returns:
+        BF (np.array): The Fourier Transform of the Blurring Kernel 
+        BFC (np.array): Conj(BF) 
+        B2F (np.array):abs(BF)**2 
+        y (np.array): The blurred image 
+    """
+    m, n = x.shape
+    m0, n0 = B.shape
+
+    Bpad = np.zeros((m, n))
+    Bpad[int((m-m0)/2):int((m+m0)/2), int((n-n0)/2):int((n+n0)/2)] = B
     Bpad = fftshift(Bpad)
-    
+
     BF = fft2(Bpad)
     BCF = np.conj(BF)
     B2F = np.abs(BF)**2
 
     y = np.real(ifft2(BF*fft2(x)))
-    
-    return BF,BCF,B2F,y
 
-def INVLS(FB,FBC,F2B,FR,tau,Nb,nr,nc):
+    return BF, BCF, B2F, y
+
+
+def INVLS(FB, FBC, F2B, FR, tau, Nb, nr, nc):
+    """[summary]
+
+    Args:
+        FB (np.array): Fourier transform of the blurring kernel B 
+        FBC (np.array): conj(FB) 
+        F2B (np.array): abs(FB)**2 
+        FR (np.array): Fourier transform of R 
+        Nb (int): scale factor Nb = dr*dc 
+        nr,nc (int,int): size of the observation 
+
+    Returns:
+        np.array: High resolution image
+    """
     x1 = FB*FR
-    FBR = blockMM(nr,nc,Nb,x1)
-    invW = blockMM(nr,nc,Nb,F2B)
+    FBR = blockMM(nr, nc, Nb, x1)
+    invW = blockMM(nr, nc, Nb, F2B)
     invWBR = FBR/(invW + tau*Nb)
-    myfun = lambda block:block*invWBR
-    FCBinvWBR = segmented_process(FBC, blk_size=(nr,nc), fun=myfun)
+    def myfun(block): return block*invWBR
+    FCBinvWBR = segmented_process(FBC, blk_size=(nr, nc), fun=myfun)
     FX = (FR-FCBinvWBR)/(tau)
     Xest = np.real(ifft2(FX))
-    
+
     return Xest
--- image_super_resolution/utils.py
+++ image_super_resolution/utils.py
@@ -9,17 +9,37 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-def gauss2D_Kernel(shape=(3,3),sigma=0.5):
-    m,n = [(ss-1.)/2. for ss in shape]
-    y,x = np.ogrid[-m:m+1,-n:n+1]
-    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
-    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+def gauss2D_Kernel(shape=(3, 3), sigma=0.5):
+    """creates a gaussian blurring kernel
+
+    Args:
+        shape (tuple, optional): shape of kernel. Defaults to (3,3).
+        sigma (float, optional): standard deviation of blurring kernel. Defaults to 0.5.
+
+    Returns:
+        np.array: bluuring kernel
+    """
+    m, n = [(ss-1.)/2. for ss in shape]
+    y, x = np.ogrid[-m:m+1, -n:n+1]
+    h = np.exp(-(x*x + y*y) / (2.*sigma*sigma))
+    h[h < np.finfo(h.dtype).eps*h.max()] = 0
     sumh = h.sum()
     if sumh != 0:
         h /= sumh
     return h
 
+
 def segmented_process(M, blk_size, fun=None):
+    """processes the image M by applying the function fun to each distinct block of size blk_size and concatenating the results into the output matrix, M_res.
+
+    Args:
+        M (np.array): input matrix
+        blk_size (tuple): size of blocks to process
+        fun (lambda function, optional): function to apply. Defaults to None.
+
+    Returns:
+        np.array: processed matrix
+    """
     rows = []
     for i in range(0, M.shape[0], blk_size[0]):
         cols = []
@@ -29,20 +49,45 @@ def segmented_process(M, blk_size, fun=None):
     M_res = np.concatenate(rows, axis=0)
     return M_res
 
-def blockMM(nr,nc,Nb,x1):
-    myfun = lambda block:block.reshape((1,nr*nc))
-    x1 = segmented_process(x1, blk_size=(nr,nc), fun=myfun)
-    x1 = x1.reshape((Nb,nr*nc))
-    x1 = x1.sum(axis = 0)
-    x = x1.reshape((nr,nc))
-    
+
+def blockMM(nr, nc, Nb, x1):
+    """decimate image
+
+    Args:
+        nr (int): size on x_axis of low_resolution image
+        nc (int): size on y_axis of low_resolution
+        Nb (int): scale factor
+        x1 (np.array): input high_resolution image
+
+    Returns:
+        np.array: decimated image
+    """
+    def myfun(block): return block.reshape((1, nr*nc))
+    x1 = segmented_process(x1, blk_size=(nr, nc), fun=myfun)
+    x1 = x1.reshape((Nb, nr*nc))
+    x1 = x1.sum(axis=0)
+    x = x1.reshape((nr, nc))
+
     return x
 
-def ISNR(y,x,x_hat):
-    return 10*np.log10(np.linalg.norm(x-y,'fro')**2/np.linalg.norm(x-x_hat,'fro')**2)
+
+def ISNR(y, x, x_hat):
+    """compute signal-to-noise ratio
+    """
+    return 10*np.log10(np.linalg.norm(x-y, 'fro')**2/np.linalg.norm(x-x_hat, 'fro')**2)
+
 
 def PSNR(img1, img2):
-    mse = np.mean( (img1 - img2) ** 2 )
+    """computes peak signal-tonoise ratio
+
+    Args:
+        img1 (np.array): image
+        img2 (np.array): image
+
+    Returns:
+        float: peak signal-tonoise ratio
+    """
+    mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
     PIXEL_MAX = 255.0
